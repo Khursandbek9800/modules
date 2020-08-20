a07cc6255bff731a1d1d4217b68876b6e0dc1635
Khursandbek <khursandbek9800@gmail.com>
2020-08-20 10:16:31 -0700

    first commit

---
+++ export_import/1.js
@@ -0,0 +1,3 @@
+import { admin } from "./admin.js";
+admin.surname = "Yusupov;"
+alert(admin.name)
---
+++ export_import/admin.js
@@ -0,0 +1,9 @@
+export let admin = {
+    name: "khursandbek",
+    age: 21,
+    sizes: {
+        height: 185 + "sm",
+        weight: 87 + "kg"
+    },
+    university: "TATU"
+}
